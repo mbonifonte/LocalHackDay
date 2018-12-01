7231e93164503ad4d86345ffdf8a67e6783da22d
Victor Chen <vchen8761@gmail.com>
2018-12-01 15:08:35 -0500

    order of sutff in scripts.js

--- scripts.js
+++ scripts.js
@@ -14,59 +14,10 @@ var words = [
     ["U", "V", "W", "X", "Y"]
 ];
 
-$(document).ready(function () {
-
-    generateTable();
-
-    // $('#btn-chat').click(function () {
-    //     var ul = document.getElementById("chat");
-    //     var li = document.createElement("li");
-    //     var msg = document.getElementById('btn-input').value;
-    //     li.appendChild(document.createTextNode(msg));
-    //     ul.appendChild(li);
-    //     alert(msg);
-    // });
-
-    function generateTable() {
-        for (i = 0; i < 5; i++) {
-            $(".words table").append('<tr id = \"row-' + (i + 1) + '\"></div>');
-            var row = $("table #row-" + i);
-            for (j = 0; j < 5; j++) {
-                row.append("<td type='" + board[i][j] + "'>" + words[i][j] + "</td>");
-            }
-        }
-    }
-
-});
-
 var redcount = 9;
 var bluecount = 8;
 var blackcount = 1;
 var graycount = 7;
-
-$(document).ready(function () {
-    $("td").each(function () {
-        $(this).prop('color', chooseColor());
-        alert($(this).attr());
-    });
-
-    $("td").click(function () {
-        //if (color = "red") {
-        //    img.src = "images\red.jpg";
-        //} else {
-        //    if (color = "blue") {
-        //        img.src = "images\blue.jpg";
-        //    } else {
-        //        if (color = "gray") {
-        //            img.src = "images\gray.jpg";
-        //        } else {
-        //            img.src = "images\black.jpg";
-        //        }
-        //    }
-        //}
-    });
-
-});
 
 function chooseColor() {
     var flag = true;
@@ -90,3 +41,38 @@ function chooseColor() {
         }
     }
 }
+
+
+$(document).ready(function () {
+
+    generateTable();
+
+    // $('#btn-chat').click(function () {
+    //     var ul = document.getElementById("chat");
+    //     var li = document.createElement("li");
+    //     var msg = document.getElementById('btn-input').value;
+    //     li.appendChild(document.createTextNode(msg));
+    //     ul.appendChild(li);
+    //     alert(msg);
+    // });
+
+    function generateTable() {
+        for (i = 0; i < 5; i++) {
+            $(".words table").append('<tr id = \"row-' + (i + 1) + '\"></div>');
+            var row = $("table #row-" + i);
+            for (j = 0; j < 5; j++) {
+                row.append("<td type='" + board[i][j] + "'>" + words[i][j] + "</td>");
+            }
+        }
+    }
+
+    $("td").each(function () {
+        $(this).prop('color', chooseColor());
+        alert($(this).attr());
+    });
+});
+
+
+
+
+
